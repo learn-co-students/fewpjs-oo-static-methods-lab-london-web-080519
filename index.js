a043b5ad68fc91301f823cfcb2b8d85af8e58761
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-10-04 13:56:57 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 class Formatter {
-  //add static methods here
+	//add static methods here
+	static capitalize(string) {
+		return string.slice(0, 1).toUpperCase() + string.slice(1);
+	}
+
+	static sanitize(string) {
+		return string.replace(/[^\w\s'-]/g, "");
+	}
+
+	static titleize(string) {
+		return string
+			.split(" ")
+			.map((item, index) => {
+				if (index == 0) {
+					return Formatter.capitalize(item);
+				} else {
+					if (
+						[
+							"the",
+							"a",
+							"an",
+							"but",
+							"of",
+							"and",
+							"for",
+							"at",
+							"by",
+							"from",
+						].includes(item)
+					) {
+						return item;
+					} else {
+						return Formatter.capitalize(item);
+					}
+				}
+			})
+			.join(" ");
+	}
 }
