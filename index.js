bd80d6f42acbc9948bbf05caa26360d5f49270b0
oliburt <oliburt@gmail.com>
2019-10-07 09:03:41 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    let regex = /[\d"\(\)<>\/\\`~!\?@#$\*\^\{\}]/g
+    return string.replace(regex, '')
+  }
+
+  static titleize(sentence) {
+     let words = sentence.split(' ')
+     let regex = /^a$|^an$|^but$|^of$|^and$|^for$|^at$|^by$|^from$|^the$/
+     return words.map((word, index) => {
+       if (index === 0 || !regex.test(word)) {
+         return this.capitalize(word)
+       } else {
+         return word
+       }
+     }).join(' ')
+  }
 }
